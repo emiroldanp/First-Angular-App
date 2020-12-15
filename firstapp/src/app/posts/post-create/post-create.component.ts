99b5af00b4c99485b11f0bc9002e5033d61794f1
EmilianoR <emiroldanp@hotmail.com>
2020-12-15 13:33:10 -0600

    Added new functionalities + model + service

--- firstapp/src/app/posts/post-create/post-create.component.ts
+++ firstapp/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
  import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +9,17 @@
 })
 
 export class PostCreateComponent {
-  enteredValue = ''
-  newPost = 'NO CONTENT'
+  enteredTitle = '';
+  enteredContent = '';
 
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  constructor(public postsService: PostsService) {
+
+  }
+
+  onAddPost(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
