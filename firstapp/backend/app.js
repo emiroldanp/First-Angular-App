e5bd27bd4c541cfb0deb5d5061b45fc7f5e0bab9
EmilianoR <emiroldanp@hotmail.com>
2020-12-16 11:19:30 -0600

    Finished with mongodb part

--- firstapp/backend/app.js
+++ firstapp/backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://emi:Tgz4N2nwwLJO6JjK@angularcluster.4fqyz.mongodb.net/<dbname>?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://emi:Tgz4N2nwwLJO6JjK@angularcluster.4fqyz.mongodb.net/angular-app?retryWrites=true&w=majority",
  {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
   console.log("Connected to MongoDB")
@@ -30,29 +30,28 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
-  res.status(201).json({
-    message: "Post added succesfully!"
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added succesfully!",
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-      id:'a01650141',
-      title: 'First server-side post',
-      content: 'This comes from the server'
-    },
-    {
-      id:'a01339939',
-      title: 'Second server-side post',
-      content: 'This comes from the server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully',
-    posts: posts
+  Post.find()
+  .then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched succesfully',
+      posts: documents
+    });
   });
+});
+
+app.delete("/api/posts/:id", (req,res,next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    res.status(200).json({message: 'Post deleted!'});
+  })
 });
 
 module.exports = app;
--- firstapp/src/app/posts/post-list/post-list.component.ts
+++ firstapp/src/app/posts/post-list/post-list.component.ts
@@ -27,6 +27,10 @@ export class PostListComponent implements OnInit, OnDestroy {
     });
   }
 
+  onDelete(postID: string) {
+    this.postsService.deletePost(postID);
+  }
+
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
