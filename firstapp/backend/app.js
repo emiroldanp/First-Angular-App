8511b4aa177a5903ac2bbeb9712f088aea2f0b5a
EmilianoR <emiroldanp@hotmail.com>
2020-12-15 19:19:42 -0600

    Finished Node part

--- firstapp/backend/app.js
+++ firstapp/backend/app.js
@@ -1,14 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added succesfully!"
+  });
+});
+
+app.get('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id:'a01650141',
+      title: 'First server-side post',
+      content: 'This comes from the server'
+    },
+    {
+      id:'a01339939',
+      title: 'Second server-side post',
+      content: 'This comes from the server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
